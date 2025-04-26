a7c93a81672749c2098f4de9f4a07b79c7375154
Shafiul007 <shafiul1426@gmail.com>
2025-04-26 21:55:27 +0600

    some style changed

--- src/components/Event/SingleEvent.jsx
+++ src/components/Event/SingleEvent.jsx
@@ -8,14 +8,10 @@ const SingleEvent = ({post,handleDelete}) => {
     return (
         <section className="dark:bg-gray-100 dark:text-gray-800">
 	<div className="container flex flex-col items-center justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-		<div className="flex flex-col space-y-5 items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+		<div className="flex flex-col items-center justify-center p-6 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
 		{ photo && <img src={photo} alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />}
 		
 
-		</div>
-		<div className="flex flex-col space-y-5 items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-		
-			
 		</div>
 		<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
 			<h1 className="text-5xl font-bold leading-none sm:text-6xl">
